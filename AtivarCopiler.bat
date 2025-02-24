fa877f9b7760aedc8130d338f455c9c2a0b34a36
LEONIDAS CAETANO DA SILVA <C3007729@fgv.edu.br>
2025-02-24 12:38:11 -0300

    Salvo 24022025

--- AtivarCopiler.bat
+++ AtivarCopiler.bat
@@ -15,13 +15,14 @@ cd /d "%PASTA%"
 
 echo Ambiente configurado!
 
-echo Diretórios adicionados
+echo Diretórios adicionados:
 echo %PENDRIVE_PATH%Apps\AppC\bin\
 echo %PENDRIVE_PATH%Apps\AppLua\5.1\
 echo %PENDRIVE_PATH%Apps\AppPython\
 echo %PENDRIVE_PATH%\Apps\AppTex\texmfs\install\miktex\bin\x64\
 echo %PENDRIVE_PATH%\Scripts\
 echo %PENDRIVE_PATH%\Apps\AppOpera\
+
 echo Pasta atual: %PASTA%
 
 cmd
