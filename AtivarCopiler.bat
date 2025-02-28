fe52db4bfb79f4cb942c94f199e606e8827a4ab3
LEONIDAS CAETANO DA SILVA <C3007729@fgv.edu.br>
2025-02-28 13:32:26 -0300

    Atualização 28/02/2025

--- AtivarCopiler.bat
+++ AtivarCopiler.bat
@@ -9,20 +9,21 @@ set "PATH=%PENDRIVE_PATH%\Apps\AppLua\5.1\;%PATH%"
 set "PATH=%PENDRIVE_PATH%\Apps\AppPython\;%PATH%"
 set "PATH=%PENDRIVE_PATH%\Apps\AppTex\texmfs\install\miktex\bin\x64\;%PATH%"
 set "PATH=%PENDRIVE_PATH%\Scripts\;%PATH%"
+set "PATH=%PENDRIVE_PATH%\Apps\AppGit\bin\;%PATH%"
 set "PATH=%PENDRIVE_PATH%\Apps\AppOpera\;%PATH%"
 
 cd /d "%PASTA%"
 
 echo Ambiente configurado!
 
-echo Diretórios adicionados:
+echo Diretórios adicionados
 echo %PENDRIVE_PATH%Apps\AppC\bin\
 echo %PENDRIVE_PATH%Apps\AppLua\5.1\
 echo %PENDRIVE_PATH%Apps\AppPython\
 echo %PENDRIVE_PATH%\Apps\AppTex\texmfs\install\miktex\bin\x64\
 echo %PENDRIVE_PATH%\Scripts\
+echo %PENDRIVE_PATH%\Apps\AppGit\bin\
 echo %PENDRIVE_PATH%\Apps\AppOpera\
-
 echo Pasta atual: %PASTA%
 
 cmd
