866fd164c8cdc8b36a44e400cdbe3d85521d8871
LEONIDAS CAETANO DA SILVA <C3007729@fgv.edu.br>
2025-08-25 09:05:07 -0300

    Just2save

--- 4-Ilustracoes/Executaveis/exemplolongquadro.py
+++ 4-Ilustracoes/Executaveis/exemplolongquadro.py
@@ -1,20 +1,29 @@
 import pandas as pd
-import long
+import long as lg
 import time
 
 try:
-    plania = pd.read_excel("../Planilhas/Quadros.xlsx", sheet_name = "LongQuadro", na_values="~")
-    
+    plania = pd.read_excel("../Planilhas/Quadros.xlsx",
+                           sheet_name="LongQuadro", na_values="~")
+
     sty = plania.style
-    
-    sty.to_longtex("../Quadros/LongQuadro.tex",
-               environment = "longquadro",
-               caption = "Exemplo de longquadro"
-               )
+
+    sty.to_latex()
+
+    sty.set_table_styles([
+        {'selector': 'toprule', 'props': ':hline;'},
+        {'selector': 'midrule', 'props': ':hline;'},
+        {'selector': 'bottomrule', 'props': ':hline;'},
+    ], overwrite=False)
+
+    a = lg.long(sty, "../Quadros/LongQuadro.tex",
+                       environment="longquadro",
+                       caption="Exemplo de longquadro"
+                       )
+    print(a)
 except FileNotFoundError as e:
-    print(f"Arquivo não encontrado!\n{e}")
+    print(f"Arquivo não encontrado!\n{e}\n")
 except Exception as e:
-    print("Error")
-    print(e)
+    print(f"Error\n{e}")
 
 time.sleep(3)
--- 4-Ilustracoes/Executaveis/long.py
+++ 4-Ilustracoes/Executaveis/long.py
@@ -1,9 +1,11 @@
-import re 
+import re
 import pandas
 from pandas.io.formats.style import Styler
 
+
 def get_columns(table: str):
-    return table[ 18 : table[18:].find("\n") + 17]
+    return table[18: table[18:].find("\n") + 17]
+
 
 def get_content(table: str):
     listofcont = []
@@ -17,66 +19,108 @@ def get_content(table: str):
             insidecontent = True
     return "\n".join(listofcont)
 
+
 def get_header(table: str):
-    firsthead_match = re.search(r"\\begin\{longtable\}\{.*?\}(.*?)\\endfirsthead", table, re.DOTALL)
+    firsthead_match = re.search(
+        r"\\begin\{longtable\}\{.*?\}(.*?)\\endfirsthead", table, re.DOTALL)
     head_match = re.search(r"\\endfirsthead(.*?)\\endhead", table, re.DOTALL)
     firsthead = firsthead_match.group(1).strip() if firsthead_match else ""
-    head = (head_match.group(1).strip() if head_match else "").replace("\\toprule", "\\midrule", 1)
+    head = (head_match.group(1).strip() if head_match else "").replace(
+        "\\toprule", "\\midrule", 1)
     return f"{firsthead}\n\\endfirsthead\n{head}\n\\endhead"
-    
+
+
 def get_length(table: str):
     match = re.search(r"\\endhead.*?\\multicolumn\{(\d+)\}", table, re.DOTALL)
-    return  match.group(1).strip() if match else "1"
+    return match.group(1).strip() if match else "1"
 
-def makefoot(fonte: str| None, continue_phrase: str| None, cols: int, hlines):
-    rules = ("\\midrule", "\\bottomrule") if hlines else ("\\hline", "\\hline")
-    foot = f"{rules[0]}\n{f"\\multicolumn{{{cols}}}{{r}}{{{continue_phrase}}}\n" if continue_phrase else ""}\\endfoot\n"
-    lastfoot = f"{rules[1]}\n\\multicolumn{{{cols}}}{{c}}{{\\fonte{"["+fonte+"]" if fonte else ""}}}\n\\endlastfoot\n"
+
+def getrules(self: Styler):
+    details = self.table_styles
+    bottom = "\\bottomrule"
+    mid = "\\midrule"
+    if not details:
+        return (mid, bottom)
+    for i in details:
+        if i["selector"] == "bottomrule":
+            bottom = "\\" + i["props"][0][1]
+        if i["selector"] == "midrule":
+            mid = "\\" + i["props"][0][1]
+    return (mid, bottom)
+
+
+def makefoot(fonte: str | None, continue_phrase: str | None, cols: int, rules: tuple):
+    foot = f"{rules[0]}\n{f"\\multicolumn{{{cols}}}{{r}}{{{continue_phrase}}}\n"\
+                        if continue_phrase else ""}\\endfoot\n"
+    lastfoot = f"{rules[1]}\n\\multicolumn{{{cols}}}{{c}}{{\\fonte{"["+fonte+"]"\
+                        if fonte else ""}}}\n\\endlastfoot\n"
     return foot + lastfoot
 
+
 def makestring(content: str, env: tuple, header: tuple):
-    head = f"\\begin{{{env[0]}}}{{{env[1]}}}\n{header[0]}\n{header[1]}" if env[0] and header[0] else ""
+    head = f"\\begin{{{env[0]}}}{{{env[1]}}}\n{header[0]}\n{header[1]}"\
+        if env[0] and header[0] else ""
     lastline = f"\n\\end{{{env[0]}}}" if env[0] and header[0] else ""
     return f"{head}{content}{lastline}"
 
+
 def exportTeX(buf: str, table: str):
-  with open(buf, "w", encoding = "utf-8") as tex:
+    with open(buf, "w", encoding="utf-8") as tex:
         tex.write(table)
 
-def long(self: Styler, buf = None, environment = None, column_format = None, caption = None, label = None, fonte = None, continue_phrase = "Continua na próxima página", ruletype = "table", **kwargs):
+
+def long(
+    self: Styler, buf: str | None = None, environment: str | None = None, column_format: str | None = None,
+    caption: str | None = None, label: str | None = None, fonte: str | None = None,
+    continue_phrase="Continua na próxima página",
+    **kwargs
+) -> str | None:
     """
     Converte uma tabela Styler em uma tabela longtable ou similar;
 
     Parameters
     ----------
         self: Objeto Styler
+        buf: Caminho para exportar, se não informado será retornado 
+        environment: tipo da tabela, se não informado será exportado somente o conteúdo
+        columns_format: formatação das colunas
 
     examples
     --------
         >>> sim
     """
-    longstr = self.to_latex(environment="longtable", caption = caption, label = label, hrules = True, **kwargs)
+    longstr = self.to_latex(environment="longtable", caption=caption,
+                            label=label, hrules=True, **kwargs)
     if not column_format:
         column_format = get_columns(longstr)
     headers = get_header(longstr)
     num_colunas = get_length(longstr)
-    foot = makefoot(fonte, continue_phrase, num_colunas, ruletype == "table")
+    ruletype = getrules(self)
+    foot = makefoot(fonte, continue_phrase, num_colunas, ruletype)
     tablecontent = get_content(longstr)
-    tablestr = makestring(tablecontent, (environment, column_format), (headers, foot))
+    tablestr = makestring(
+        tablecontent, (environment, column_format), (headers, foot))
     if not buf:
         return tablestr
     exportTeX(buf, tablestr)
+    return None
 
-def tex(self: Styler, buf = None, environment = None, 
-         column_format = None, caption = None, label = None,
-         fonte = None, position = None, **kwargs):
-    tablestr = self.to_latex(environment = environment, column_format = column_format, caption = caption, label = label, **kwargs)
+
+def tex(
+    self: Styler, buf: str | None, environment: str | None,
+    column_format: str | None, caption: str | None, label: str | None,
+    fonte: str | None, position: str | None, **kwargs
+):
+    tablestr = self.to_latex(environment=environment,
+                             column_format=column_format,
+                             caption=caption, label=label, position=position, **kwargs)
     tablelist = tablestr.splitlines()
     tablelist.insert(-1, "\n\\fonte" + (f"[{fonte}]" if fonte else ""))
     tablestr = "\n".join(tablelist)
     if not buf:
         return tablestr
     exportTeX(buf, tablestr)
-    
+
+
 Styler.to_longtex = long
 Styler.to_tex = tex
